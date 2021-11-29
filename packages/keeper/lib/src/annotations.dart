1f8efd3be29a33b513e3f6c153febc1b276972a1
André Baltazar <me@andrebaltazar.com>
2021-11-29 18:33:44 +0000

    improvement: add some more docs to annotations

--- packages/keeper/lib/src/annotations.dart
+++ packages/keeper/lib/src/annotations.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:keeper/src/keys.dart';
 
 /// Declares that a class contains keys that should be kept.
@@ -5,13 +6,19 @@ class MakeKept {
   const MakeKept._();
 }
 
+/// Declares that a class contains keys that should be kept.
 const kept = MakeKept._();
 
 /// Declares a class field as kept at [key]. For sync storage.
 ///
 /// For async storage use [AtAsync].
 class At {
+  /// A function that provides the key to be used for storage.
   final KeepKey Function() key;
+
+  /// Declares a class field as kept at [key]. For sync storage.
+  ///
+  /// For async storage use [AtAsync].
   const At(this.key);
 }
 
@@ -19,6 +26,11 @@ class At {
 ///
 /// For sync storage use [At].
 class AtAsync {
+  /// A function that provides the key to be used for async storage.
   final KeepAsyncKey Function() key;
+
+  /// Declares a class field as kept at [key]. For async storage.
+  ///
+  /// For sync storage use [At].
   const AtAsync(this.key);
 }
